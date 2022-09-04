d95636a2759ecc2a38c3961470536225ddada76b
Chukwuebuka2 <nwokporochukwuebuka@gmail.com>
2022-09-04 09:33:58 +0100

    Added Procfile

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SKEY')
+# SECRET_KEY = config('SKEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -138,8 +138,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = config('EMAIL_USER')
-DEFAULT_FROM_EMAIL = config('FROM_EMAIL')
-EMAIL_HOST_PASSWORD=config('EMAIL_PASSWORD')
+# EMAIL_HOST_USER = config('EMAIL_USER')
+# DEFAULT_FROM_EMAIL = config('FROM_EMAIL')
+# EMAIL_HOST_PASSWORD=config('EMAIL_PASSWORD')
 EMAIL_TIMEOUT = 10
 EMAIL_USE_TLS = True
