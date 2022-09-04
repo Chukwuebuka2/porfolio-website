9d7d831864dad22832ec596471f51918f7048f7f
Chukwuebuka2 <nwokporochukwuebuka@gmail.com>
2022-09-04 09:47:37 +0100

    Added Procfile

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -143,3 +144,5 @@ EMAIL_PORT = 587
 # EMAIL_HOST_PASSWORD=config('EMAIL_PASSWORD')
 EMAIL_TIMEOUT = 10
 EMAIL_USE_TLS = True
+
+django_heroku.settings(locals())
