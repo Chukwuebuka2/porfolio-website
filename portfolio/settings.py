3936e03afa192ef4fa43df99f0c3922b3a480f16
Chukwuebuka2 <nwokporochukwuebuka@gmail.com>
2022-09-04 08:57:32 +0100

    Added Procfile

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -136,10 +136,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root/')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'nwokporochukwuebuka@gmail.com'
-# DEFAULT_FROM_EMAIL = 'nwokporochukwuebuka@gmail.com'
-EMAIL_HOST_PASSWORD='jjlfucgyltgozzde'
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+DEFAULT_FROM_EMAIL = os.environ.get('FROM_EMAIL')
+EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_PASSWORD')
 EMAIL_TIMEOUT = 10
 EMAIL_USE_TLS = True
