b6270df49c89a9815ef1dbd221f8d5f6e3fd91cb
Chukwuebuka2 <nwokporochukwuebuka@gmail.com>
2022-09-04 11:20:56 +0100

    Added Procfile

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('ebuka/', admin.site.urls),
     path('', include('ebuka.urls'))
 ]
 
